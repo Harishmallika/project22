c2b45f672f0904a638a7638e80b1fa624064bbc1
Harishmallika <gharishece74@gmail.com>
2023-12-12 09:44:59 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def bootstrap2(request):
+    return render(request,'bootstrap2.html')
+def bootstrap3(request):
+    return render(request,'bootstrap3.html')
+def bootstrap4(request):
+    return render(request,'bootstrap4.html') 
+def bootstrap5(request):
+    return render(request,'bootstrap5.html')   
+def bootstrap6(request):
+    return render(request,'bootstrap6.html') 
+def badge1(request):
+    return render(request,'badge1.html')   
+def badge2(request):
+    return render(request,'badge2.html')     
+def badge3(request):
+    return render(request,'badge3.html')  
+def badge4(request):
+    return render(request,'badge4.html')                     
+    
